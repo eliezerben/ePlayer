1a9d745207a0126b6b7d7fb84c558e83a6dcdab9
Eliezer Benjamin <eliezerben96@gmail.com>
2023-08-18 16:05:40 +0530

    Refactor to move example to separate directory

---
+++ example/app.js
@@ -0,0 +1,56 @@
+var playlist_1 = [
+    { 
+        url: "https://eliezerben.github.io/ePlayer/music/music1.mp3",
+        name: "Mountain Sun"        
+    },{
+        url: "https://eliezerben.github.io/ePlayer/music/music2.mp3",
+        name: "Walk In The Park"
+    },{
+        url: "https://eliezerben.github.io/ePlayer/music/music3.mp3",
+        name: "Nice And Easy"
+    },{
+        url: "https://eliezerben.github.io/ePlayer/music/music4.mp3",
+        name: "Hoedown"
+    },{
+        url: "https://eliezerben.github.io/ePlayer/music/music5.mp3",
+        name: "12 Mornings"
+    }
+];
+var options_1 = {
+    playlist: true,
+    repeat: 'on',
+    shuffle: 'on',
+    volume: 0.7,
+    autoplay: false,
+    preload: 'auto'
+};
+var p1 = ePlayer.darkTheme('p1', options_1, playlist_1);
+
+
+var playlist_2 = [
+    { 
+        url: "https://eliezerben.github.io/ePlayer/music/music1.mp3",
+        name: "Mountain Sun"        
+    },{
+        url: "https://eliezerben.github.io/ePlayer/music/music2.mp3",
+        name: "Walk In The Park"
+    },{
+        url: "https://eliezerben.github.io/ePlayer/music/music3.mp3",
+        name: "Nice And Easy"
+    },{
+        url: "https://eliezerben.github.io/ePlayer/music/music4.mp3",
+        name: "Hoedown"
+    },{
+        url: "https://eliezerben.github.io/ePlayer/music/music5.mp3",
+        name: "12 Mornings"
+    }
+];
+var options_2 = {
+    playlist: true,
+    repeat: 'off',
+    shuffle: 'off',
+    volume: 0.5,
+    autoplay: false,
+    preload: 'auto'
+};
+var p2 = ePlayer.darkTheme('p2', options_2, playlist_2);
